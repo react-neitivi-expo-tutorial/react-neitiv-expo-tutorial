f0308699389c2d8c5a3cd966b84481f6e0dd3b69
kaxam1 <kaxam1@gmail.com>
2020-01-31 20:18:25 +0400

    remove alert from todoScreen

--- App.js
+++ App.js
@@ -1,46 +1,71 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList, ToolbarAndroid } from 'react-native';
-import { Navbar } from './src/components/Navbar'
-import { MainScreen } from './src/screens/MainScreen'
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import { Navbar } from './src/components/Navbar';
+import { MainScreen } from './src/screens/MainScreen';
 import { TodoScreen } from './src/screens/TodoScreen';
 
 export default function App() {
-  const [todoId, setTodosId] = useState('1')
+  const [todoId, setTodosId] = useState('1');
   const [todos, setTodos] = useState([
-    {id:'1',title:"პირველი პოსტი"},
-    {id:'2',title:"მეორე პოსტი"}
-  ])
-  const addTodo = (title) => {
-   setTodos(prev => [
-     ...prev, 
-     {
-    id: Date.now().toString(),
-    title: title
-    }
-    ])
-  }
+    { id: '1', title: 'პირველი პოსტი' },
+    { id: '2', title: 'მეორე პოსტი' }
+  ]);
+  const addTodo = title => {
+    setTodos(prev => [
+      ...prev,
+      {
+        id: Date.now().toString(),
+        title: title
+      }
+    ]);
+  };
 
   const removeTodo = id => {
-   setTodos(prev => prev.filter(todo => todo.id !== id))
-  }
-  let content = (<MainScreen
-  todos={todos} 
-  addTodo={addTodo}
-  removeTodo={removeTodo}
-  openTodo={setTodosId}
-/>)
+    const todo = todos.find(t => t.id === id);
+    Alert.alert(
+      'ელემენტის წაშლა',
+      `დარწმუნებული ხართ რომ გსურთ წაშლა "${todo.title}" ?`,
+      [
+        {
+          text: 'უარყოფა',
+          style: 'cancel'
+        },
+        {
+          text: 'წაშლა',
+          style: 'destructive',
+          onPress: () => {
+            setTodosId(null);
+            setTodos(prev => prev.filter(todo => todo.id !== id));
+          }
+        }
+      ],
+      { cancelable: false }
+    );
+  };
+  let content = (
+    <MainScreen
+      todos={todos}
+      addTodo={addTodo}
+      removeTodo={removeTodo}
+      openTodo={setTodosId}
+    />
+  );
 
-  if(todoId){
-    const selectedTodo = todos.find(todo=> todo.id === todoId)
-    content = (<TodoScreen title={selectedTodo} goBack={()=>setTodosId(null)}/>)
+  if (todoId) {
+    const selectedTodo = todos.find(todo => todo.id === todoId);
+    content = (
+      <TodoScreen
+        onRemove={removeTodo}
+        title={selectedTodo}
+        goBack={() => setTodosId(null)}
+      />
+    );
   }
 
   return (
-    <View >
+    <View>
       <Navbar title="Todo App!" />
-        <View style={styles.container}>
-          {content}
-        </View>
+      <View style={styles.container}>{content}</View>
     </View>
   );
 }
@@ -49,5 +74,5 @@ const styles = StyleSheet.create({
   container: {
     paddingHorizontal: 30,
     paddingVertical: 20
-  },
+  }
 });
--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,40 +1,44 @@
-import React from 'react'
-import {View, Text, Button, StyleSheet} from 'react-native'
-import {THEME} from '../theme'
-import { AppCard } from '../ui/AppCard'
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { THEME } from '../theme';
+import { AppCard } from '../ui/AppCard';
 
-export const TodoScreen = ({title,goBack}) =>{
-    return (
-        <View>
-            <AppCard style={styles.card}>
-                  <Text style={styles.title}>{title.title}</Text>
-                  <Button title='რედ.' />
-            </AppCard>
-            <View style={styles.buttons}>
-                <View style={styles.button}>
-                     <Button title='უკან' color={THEME.GREY_COLOR} onPress={goBack} />
-                </View>
-                <View style={styles.button}>
-                    <Button title='წაშლა' color={THEME.DANGER_COLOR} onPress={()=>console.log('To Remove')} />
-                </View>
-            </View>
+export const TodoScreen = ({ title, goBack, onRemove }) => {
+  return (
+    <View>
+      <AppCard style={styles.card}>
+        <Text style={styles.title}>{title.title}</Text>
+        <Button title="რედ." />
+      </AppCard>
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="უკან" color={THEME.GREY_COLOR} onPress={goBack} />
         </View>
-    )
-}
+        <View style={styles.button}>
+          <Button
+            title="წაშლა"
+            color={THEME.DANGER_COLOR}
+            onPress={() => onRemove(title.id)}
+          />
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    buttons:{
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    card:{
-        marginBottom: 20,
-        padding: 15
-    },
-    button:{
-        width: '40%'
-    },
-    title: {
-        fontSize: 20
-    }
-})
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  card: {
+    marginBottom: 20,
+    padding: 15
+  },
+  button: {
+    width: '40%'
+  },
+  title: {
+    fontSize: 20
+  }
+});
